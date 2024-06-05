441c63f5259d84547d1056cf4ea318e58e3a38df
Hai Dang <contact@hdang09.tech>
2024-06-05 08:50:37 +0700

    feat: init campaign list page

---
+++ app/dashboard/campaign/page.tsx
@@ -0,0 +1,10 @@
+export default function Page() {
+  return (
+    <>
+      <h1>Campaign Page</h1>
+      <h2>
+        Get started by editing <strong>app/dashboard/campaign/page.tsx</strong>
+      </h2>
+    </>
+  )
+}
