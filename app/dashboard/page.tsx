14363c64459ce0c69ebd7054aa601ae9929e1a9f
Hai Dang <contact@hdang09.tech>
2024-06-19 19:42:42 +0700

    fix: fix conflict with branch dev

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,10 @@
+export default function Page() {
+  return (
+    <>
+      <h1>Dashboard Page</h1>
+      <h2>
+        Get started by editing <strong>app/dashboard/page.tsx</strong>
+      </h2>
+    </>
+  )
+}
