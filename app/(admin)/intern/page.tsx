f17d8b6155837071129efaba41d83cdfa2db2e12
Hai Dang <contact@hdang09.tech>
2024-07-16 00:12:40 +0700

    style: update grid on mobile

--- app/(admin)/intern/page.tsx
+++ app/(admin)/intern/page.tsx
@@ -63,7 +63,7 @@ export default function Page() {
         </Link>
       </div>
 
-      <div className="mt-8 grid grid-cols-4 gap-6">
+      <div className="mt-8 grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-4">
         {filteredInternship.map((intern) => (
           <div
             key={intern.userId}
